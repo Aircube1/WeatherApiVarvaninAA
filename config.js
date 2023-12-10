fc771e736d55ff802d982294e306e515962ae803
Aircube1 <varvanin.2002@gmail.com>
2023-12-10 15:36:39 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric",
+        SECRET_KEY: "ваш ключ"
+    }
+}
+
+module.exports = constants
